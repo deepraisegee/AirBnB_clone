ae285cdf7dabdd0ba53ff824b58cefc55033ac8f
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-05-15 03:32:02 +0000

    feat: add the base model for all the model classes

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Decribes all the base model classes to be used for the program
+"""
+
+
+class BaseModel(object):
+    """
+    BaseModel that defines all common attributes
+    and amethods for other classes.
+    """
+
+    pass
