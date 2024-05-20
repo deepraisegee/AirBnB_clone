9553f6b194cd5c391e00ea020c584a53b913e8b9
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-05-20 04:54:58 +0000

    add the show command

--- console.py
+++ console.py
@@ -11,6 +11,9 @@ Usage:
 import cmd
 import sys
 
+import models
+from models.engine.file_storage import FileStorage
+
 
 class HBNBCommand(cmd.Cmd):
     """The processor class for all the commads"""
@@ -18,16 +21,58 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def emptyline(self):
-        """do nothing when the line is empty"""
+        """Handle when the line is empty"""
         pass
 
-    def do_EOF(self, line):
-        """end of file"""
+    def do_EOF(self, arg):
+        """Handle end of file"""
         return True
 
-    def do_quit(self, line):
-        """Quit command to exit the program"""
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
         sys.exit(0)
+
+    def do_create(self, arg):
+        """
+        Create new instance of a model.
+        Usage:
+            create <ModelName>
+        """
+        if not arg.strip():
+            print("** class name missing **")
+            return
+        if arg not in models.CLASSES:
+            print("** class doesn't exist **")
+            return
+        obj = models.CLASSES[arg]()
+        obj.save()
+        print(obj.id)
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an
+        instance based on the class name and id.
+        Usage:
+            show <ModelName> <instance_id>
+        """
+        args = arg.split(" ")
+        if not arg.strip():
+            print("** class name missing **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        if len(args) == 2 and args[0] not in models.CLASSES:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 2:
+            try:
+                class_name = args[0]
+                obj_id = f"{class_name}.{args[1]}"
+                print(models.storage.all()[obj_id])
+            except KeyError:
+                print("** no instance found **")
+            return
 
 
 if __name__ == "__main__":
--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
+CLASSES = {
+    "BaseModel": BaseModel
+}
 
 storage = FileStorage()
 storage.reload()
